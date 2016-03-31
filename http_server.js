64a38e9c69ef350ea694f397c7499551bba157c4
Zack Wang <zackwang1218@gmail.com>
2016-03-31 23:43:02 +1100

    add write stream for post request

--- http_server.js
+++ http_server.js
@@ -1,10 +1,19 @@
 var fs = require('fs');
 var http = require('http');
 var portNum = 8080;
+var portNum2 = 8081;
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'content-type':'text/plain'});
-    fs.createStream('./readme.txt').pipe(res);
+    fs.createReadStream('./readme.txt').pipe(res);
     console.log('done!');
 })
 server.listen(portNum);
+
+var server2 = http.createServer(function(req, res){
+	if (req.method ==='POST'){
+		req.pipe(fs.createWriteStream(data/mydata.txt));
+	}
+	res.end('finished')
+})
+server2.listen(portNum2);
